fccce2b00abfda35347568cf75d89b21fc6deffa
Dave Laffan <dave.laffan@outlook.com>
2020-05-21 14:32:02 +0100

    Remove 'quizzed' project from projects section

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
     // - See More Projects Button - //
     $("#btn-see-more").on("click", function () {
-        $("#hidden-projects").slideToggle(700, function () {
+        $("#hidden-projects").slideToggle(500, function () {
             $("#btn-see-more").toggleHtml(
                 'See More Projects',
                 '<i class="fas fa-angle-double-up fa-2x"></i>'
