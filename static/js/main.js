fb8b298371f3f81ec696a309c19887ceebf19789
Dave Laffan <dave.laffan@outlook.com>
2020-05-21 15:43:23 +0100

    Add footer

--- static/js/main.js
+++ static/js/main.js
@@ -9,11 +9,14 @@ $(document).ready(function () {
     $("#btn-see-more").on("click", function () {
         $("#hidden-projects").slideToggle(500, function () {
             $("#btn-see-more").toggleHtml(
-                'See More Projects',
+                "See More Projects",
                 '<i class="fas fa-angle-double-up fa-2x"></i>'
             );
         });
     });
 
-    tippy('[data-tippy-content]');
+    tippy("[data-tippy-content]");
+
+    // ----------------------------------------------- Insert Copyright Year //
+    $("#copyright-year").text(new Date().getFullYear());
 });
