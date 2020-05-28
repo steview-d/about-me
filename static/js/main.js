1dbf10d709aea44c6f9527b8f0b3d296b87363cb
Dave Laffan <dave.laffan@outlook.com>
2020-05-28 09:11:41 +0100

    Update navbar

--- static/js/main.js
+++ static/js/main.js
@@ -10,13 +10,18 @@ $(document).ready(function () {
         $("#hidden-projects").slideToggle(500, function () {
             $("#btn-show-more").toggleHtml(
                 "Show More Projects",
-                'Show Fewer Projects'
+                "Show Fewer Projects"
             );
         });
     });
 
     tippy("[data-tippy-content]");
 
-    // ----------------------------------------------- Insert Copyright Year //
+    // - Insert Copyright Year - //
     $("#copyright-year").text(new Date().getFullYear());
+
+    // - close mobile nav menu on click - //
+    $(".navbar-nav>li>a").on("click", function () {
+        $(".navbar-collapse").collapse("hide");
+    });
 });
